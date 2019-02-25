6aa87f568f1749d709cafc34280117529da25641
Igor <igor.gnatenko555@gmail.com>
2019-02-25 17:44:43 +0200

    Это мой 8 комит

--- project 3/server.php
+++ project 3/server.php
@@ -1,3 +1,15 @@
-<?php 
+$user = 'root';
+$password = 'root';
+$db = 'inventory';
+$host = 'localhost';
+$port = 3306;
 
-echo $_SERVER[‘REMOTE_ADDR‘];
+$link = mysql_connect(
+   "$host:$port", 
+   $user, 
+   $password
+);
+$db_selected = mysql_select_db(
+   $db, 
+   $link
+);
